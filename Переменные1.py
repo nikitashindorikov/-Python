ef8d1a9196740c57f32a060ee6e4284015a2b970
nikitashindorikov <shindorikovjk@gmail.com>
2024-08-14 21:33:39 +0300

    Delete Переменные1.py

--- Переменные1.py
+++
@@ -1,7 +0,0 @@
-Number_of_completed_DZ = "12"
-print(Number_of_completed_DZ)
-Number_of_hours_spend = "1.5"
-print(Number_of_hours_spend)
-Title_of_the_lesson = "Python"
-Time_of_one_task = [125/1000]
-print(Time_of_one_task)
