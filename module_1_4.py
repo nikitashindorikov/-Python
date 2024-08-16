7d8929a6a1627ccd5b9deaa237f2e2871ed19c83
nikitashindorikov <shindorikovjk@gmail.com>
2024-08-16 22:17:46 +0300

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,8 +0,0 @@
-my_string = input("Меня зовут Никита, а как зовут вас? ")
-print(my_string)
-my_string = "Меня зовут Никита"
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
