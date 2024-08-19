e188b414b1f369e4a40046176d62c004b127be02
nikitashindorikov <shindorikovjk@gmail.com>
2024-08-19 23:15:47 +0300

    Add files via upload

---
+++ все ли равно.py
@@ -0,0 +1,25 @@
+first = int(10)
+second = int(10)
+third = int(10)
+if first == second == third:
+    print(3)
+first = int(32)
+second = int(43)
+third = int(32)
+if first == second == third:
+    print(0)
+elif first == second:
+    print(0)
+elif first == third:
+    print(2)
+first = 56
+second =74
+third = 43
+if first == second == third:
+    print(3)
+elif first == second:
+    print(2)
+else:
+    print(0)
+
+
